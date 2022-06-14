56d8f33cb61e4576156c42cdea4857a647fc5cc3
foxnorth228 <khitrii03@gmail.com>
2022-06-14 22:45:07 +0300

    feat: create test class

--- tests/test.ts
+++ tests/test.ts
@@ -1,11 +1,93 @@
 import { sendRequestToServer } from "./testFunc.js";
 import { v4 } from "uuid";
 
-const testRequestFunc = (object: object={}, method: string="GET", path:string="api/users") => 
-  sendRequestToServer("localhost", 3000, path, object, method);
+async function testRequestFunc(object: object={}, method: string="GET",
+ path: string="api/users"): Promise<[number, string]> {
+    const [status, body] = await sendRequestToServer("localhost", 3000, path, method);
+    const reader = body.getReader();
+    let receivedLength = 0;
+    let chunks = []; 
+    while(true) {
+        const {done, value} = await reader.read();
+        if (done) {
+            break;
+        }
+        chunks.push(value);
+        receivedLength += value.length;
+    }
+    let chunksAll = new Uint8Array(receivedLength); // (4.1)
+    let position = 0;
+    for(let chunk of chunks) {
+        chunksAll.set(chunk, position); // (4.2)
+        position += chunk.length;
+    }
+    let result = new TextDecoder("utf-8").decode(chunksAll);
+    return [status, JSON.parse(result)];
+}
+
+type basicTypes = boolean | number | string | object;
+
+class Test {
+    private static numTests = 1;
+    private static succeedTests = 0;
+    private static failedTests = 0;
+
+    private static failedTestElement(arr: basicTypes[], answer: basicTypes[], num: number) {
+        console.log("***");
+        console.log(`Test ${Test.numTests} failed`);
+        console.log(`${arr.join(", ")} is not like ${answer.join(", ")}`);
+        console.log(`${JSON.stringify(arr[num])} is not like ${JSON.stringify(answer[num])}`);
+        console.log("***");
+        Test.numTests++;
+        Test.failedTests++;
+    }
+
+    private static failedTestCount(arr: basicTypes[], answer: basicTypes[]) {
+        console.log("***");
+        console.log(`Test ${Test.numTests} failed`);
+        console.log(`Number of testing elements is ${arr.length}, but number elements in answer is ${answer.length}`);
+        console.log("***");
+        Test.numTests++;
+        Test.failedTests++;
+    }
+
+    private static succeedTest() {
+        Test.numTests++;
+        Test.succeedTests++;
+    }
+
+    private static errorTest(err: unknown) {
+        if(err instanceof Error) {
+            console.log(`In ${Test.numTests} test catch error: ${err.message}`);
+        }
+        Test.numTests++;
+        Test.failedTests++;
+    }
+
+    static async testAsyncFunction(func: Function, arr: basicTypes[], ...args: basicTypes[]) {
+        try { 
+            const answer = await func(args); 
+            if (answer.length != arr.length) {
+                Test.failedTestCount(arr, answer);
+                return;
+            }
+            for (let i = 0; i < answer.length; ++i) {
+                if (arr[i] === answer[i]) {
+                    continue;
+                } else {
+                    Test.failedTestElement(arr, answer, i);
+                    return;
+                }
+            }
+            Test.succeedTest();
+        } catch(err) {
+            Test.errorTest(err);
+        }
+    }
+}
 
 async function checkIfServerWork() {
-    await testRequestFunc({}, "GET");
+    await Test.testAsyncFunction(testRequestFunc, [404, {}], {}, "GET");
 }
 
 async function workWithSingleUser() { 
@@ -59,6 +141,6 @@ async function negativeTests() {
 }
 
 checkIfServerWork();
-workWithSingleUser();
-workWithMultipleUsers();
-negativeTests();
+//workWithSingleUser();
+//workWithMultipleUsers();
+//negativeTests();
--- tests/testFunc.ts
+++ tests/testFunc.ts
@@ -1,22 +1,14 @@
 import { v4 } from "uuid";
 //type user = {id: };
 
-export async function sendRequestToServer(server: string="localhost", 
-  port:number=3000, path: string="api/users", obj: object, method: string): Promise<Object> {
-    const url = `http://${server}:${port}/${path}`;
-    let object: Object = {};
-    try{
-        const answer = await fetch(url, {
-            method: method,
-        });
-        object = await answer.json();
-        console.log(object);
-    } catch(err) {
-        if(err instanceof Error) {
-            console.log(err.message);
-            object = err;
-        }
-    }
-    console.log(v4());
-    return object;
+interface IStatus {
+    status: number;
+    body: ReadableStream;
+}
+
+export async function sendRequestToServer(hostname: string="localhost", port:number=3000, 
+  path: string="api/users", method: string): Promise<[number, ReadableStream]> {
+    const url = `http://${hostname}:${port}/${path}`;
+    const answer = await fetch(url, { method: method }) as IStatus;
+    return [answer.status, answer.body];
 }
